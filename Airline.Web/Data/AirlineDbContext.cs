f01ea900347f9e3e88ade9a01bae25979123e861
ZENOTI\divyeshb <divyeshb@zenoti.com>
2021-10-18 17:47:55 +0530

    Added Migrations.

--- Airline.Web/Data/AirlineDbContext.cs
+++ Airline.Web/Data/AirlineDbContext.cs
@@ -1,16 +1,12 @@
 ﻿using AirlineWeb.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AirlineWeb.Data
 {
     public class AirlineDbContext : DbContext
     {
         public AirlineDbContext(DbContextOptions<AirlineDbContext> opt) : base(opt) { }
-        
+
         public DbSet<WebhookSubscription> WebhookSubscriptions { get; set; }
         public DbSet<FlightDetail> FlightDetails { get; set; }
     }
---
+++ Airline.Web/Migrations/20211018115700_WebHooksInitial.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AirlineWeb.Migrations
+{
+    public partial class WebHooksInitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FlightDetails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FlightCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FlightDetails", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WebhookSubscriptions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    WebhookURI = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Secret = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    WebhookType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    WebhookPublisher = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WebhookSubscriptions", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FlightDetails");
+
+            migrationBuilder.DropTable(
+                name: "WebhookSubscriptions");
+        }
+    }
+}
