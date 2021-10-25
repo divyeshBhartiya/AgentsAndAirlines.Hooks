9dceeef79332e6d59ffa7dd47505044ca526f5c1
ZENOTI\divyeshb <divyeshb@zenoti.com>
2021-10-25 22:33:34 +0530

    AirlineWeb: Added endpoints for Flights and Webhook Subscription.

--- Airline.Web/Controllers/FlightsController.cs
+++ Airline.Web/Controllers/FlightsController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AirlineWeb.Data;
+using AirlineWeb.Dtos;
+using AirlineWeb.MessageBus;
+using AirlineWeb.Models;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,5 +16,80 @@ namespace AirlineWeb.Controllers
     [ApiController]
     public class FlightsController : ControllerBase
     {
+        private readonly AirlineDbContext _context;
+        private readonly IMapper _mapper;
+        private readonly IMessageBusClient _messageBus;
+
+        public FlightsController(AirlineDbContext context, IMapper mapper, IMessageBusClient messageBus)
+        {
+            _context = context;
+            _mapper = mapper;
+            _messageBus = messageBus;
+        }
+
+        [HttpGet("{flightCode}", Name = "GetFlightDetailsByCode")]
+        public ActionResult<FlightDetailReadDto> GetFlightDetailsByCode(string flightCode)
+        {
+            var flight = _context.FlightDetails.FirstOrDefault(f => f.FlightCode == flightCode);
+            if (flight == null) return NotFound();
+
+            //This mapping won't work as I have not done the Profiles section Duh!!!
+            return Ok(_mapper.Map<FlightDetailReadDto>(flight));
+        }
+
+        [HttpPost]
+        public ActionResult<FlightDetailReadDto> CreateFlight(FlightDetailCreateDto flightDetailCreateDto)
+        {
+            var flight = _context.FlightDetails.FirstOrDefault(f => f.FlightCode == flightDetailCreateDto.FlightCode);
+
+            if (flight == null)
+            {
+                var flightDetailModel = _mapper.Map<FlightDetail>(flightDetailCreateDto);
+                try
+                {
+                    _context.FlightDetails.Add(flightDetailModel);
+                    _context.SaveChanges();
+                }
+                catch (Exception ex) 
+                { 
+                    return BadRequest(ex.Message); 
+                }
+                var flightDetailReadDto = _mapper.Map<FlightDetailReadDto>(flightDetailModel);
+                return CreatedAtRoute(nameof(GetFlightDetailsByCode), 
+                    new { flightCode = flightDetailReadDto.FlightCode }, flightDetailReadDto);
+            }
+            else return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateFlightDetail(int id, FlightDetailUpdateDto flightDetailUpdateDto)
+        {
+            var flight = _context.FlightDetails.FirstOrDefault(f => f.Id == id);
+            
+            if (flight == null) return NotFound();
+
+            decimal oldPrice = flight.Price;
+            _mapper.Map(flightDetailUpdateDto, flight);
+            
+            try
+            {
+                _context.SaveChanges();
+                if (oldPrice != flight.Price)
+                {
+                    Console.WriteLine("Price Changed - Place message on bus");
+                    var message = new NotificationMessageDto
+                    {
+                        WebhookType = "pricechange",
+                        OldPrice = oldPrice,
+                        NewPrice = flight.Price,
+                        FlightCode = flight.FlightCode
+                    };
+                    _messageBus.SendMessage(message);
+                }
+                else Console.WriteLine("No Price change");
+                return NoContent();
+            }
+            catch (Exception ex) { return BadRequest(ex.Message); }
+        }
     }
 }
---
+++ Airline.Web/Controllers/WebhookSubscriptionController.cs
@@ -0,0 +1,59 @@
+﻿using AirlineWeb.Data;
+using AirlineWeb.Dtos;
+using AirlineWeb.Models;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirlineWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WebhookSubscriptionController : ControllerBase
+    {
+        private readonly AirlineDbContext _context;
+        private readonly IMapper _mapper;
+        public WebhookSubscriptionController(AirlineDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet("{secret}", Name = "GetSubscriptionBySecret")]
+        public ActionResult<WebhookSubscriptionReadDto> GetSubscriptionBySecret(string secret)
+        {
+            var subscription = _context.WebhookSubscriptions.FirstOrDefault(s => s.Secret == secret);
+            if (subscription == null) return NotFound();
+
+            return Ok(_mapper.Map<WebhookSubscriptionReadDto>(subscription));
+        }
+
+        [HttpPost]
+        public ActionResult<WebhookSubscriptionReadDto> CreateSubsription(WebhookSubscriptionCreateDto webhookSubscriptionCreateDto)
+        {
+            var subscription = _context.WebhookSubscriptions.FirstOrDefault(s => s.WebhookURI == webhookSubscriptionCreateDto.WebhookURI);
+
+            if (subscription == null)
+            {
+                subscription = _mapper.Map<WebhookSubscription>(webhookSubscriptionCreateDto);
+                subscription.Secret = Guid.NewGuid().ToString();
+                subscription.WebhookPublisher = "PanAus";
+                try
+                {
+                    _context.WebhookSubscriptions.Add(subscription);
+                    _context.SaveChanges();
+                }
+                catch (Exception ex) { return BadRequest(ex.Message); }
+
+                var webhookSubscriptionReadDto = _mapper.Map<WebhookSubscriptionReadDto>(subscription);
+                return CreatedAtRoute(nameof(GetSubscriptionBySecret), 
+                    new { secret = webhookSubscriptionReadDto.Secret }, webhookSubscriptionReadDto);
+            }
+            else return NoContent();
+        }
+    }
+}
