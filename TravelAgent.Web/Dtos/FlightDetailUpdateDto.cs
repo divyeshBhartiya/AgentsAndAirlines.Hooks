d4eb55aaa3d301e3008d09b5c78c2fe2aa6583b1
ZENOTI\divyeshb <divyeshb@zenoti.com>
2021-10-27 01:34:40 +0530

    TravelAgentWeb: Added Models and DTOs

---
+++ TravelAgent.Web/Dtos/FlightDetailUpdateDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelAgentWeb.Dtos
+{
+    public class FlightDetailUpdateDto
+    {
+        public string Publisher { get; set; }
+        public string Secret { get; set; }
+        public string FlightCode { get; set; }
+        public decimal OldPrice { get; set; }
+        public decimal NewPrice { get; set; }
+        public string WebhookType { get; set; }
+    }
+}
