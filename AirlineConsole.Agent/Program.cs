0596568079eb4e9d0176ef4d2684ece9ce141454
ZENOTI\divyeshb <divyeshb@zenoti.com>
2021-10-28 23:02:42 +0530

    AirlineAgentConsole: Added Dependency Injection and AppHost.

--- AirlineConsole.Agent/Program.cs
+++ AirlineConsole.Agent/Program.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using AirlineAgentConsole.App;
+using AirlineAgentConsole.Client;
+using AirlineAgentConsole.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
 
 namespace AirlineAgentConsole
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddUserSecrets(typeof(Program).Assembly, optional: true)
+                .AddEnvironmentVariables()
+                .AddCommandLine(args);
+
+            var config = builder.Build();
+
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddSingleton<IAppHost, AppHost>();
+                    services.AddSingleton<IWebhookClient, WebhookClient>();
+                    services.AddDbContext<SendAgentDbContext>(opt => opt.UseSqlServer("Server=DIVYESHB-LP\\SQLSERVER;Database=AirlineWebDB;User Id=sa;Password=Titan#12"));
+                    //(config.GetConnectionString("AirlineConnection")));
+
+                    services.AddHttpClient();
+                }).Build();
+
+            host.Services.GetService<IAppHost>().Run();
         }
     }
 }
